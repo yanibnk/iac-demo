45a83fd71b621fcdaeee0ceff75289a3947f892b
yanibnk <135028223+yanibnk@users.noreply.github.com>
2025-05-07 12:51:06 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,7 +21,7 @@ resource "random_id" "commit_simulation" {
 }
 
 resource "aws_instance" "demo" {
-  ami           = "ami-${random_id.commit_simulation.hex}" # fictif mais uniquejbjjkbbijbjkhfftyhfj
+  ami           = "ami-${random_id.commit_simulation.hex}" # fictif mais uniquejbjjkbbijbjkhfftyhfjjh
   instance_type = "t2.micro"
 
   tags = {
