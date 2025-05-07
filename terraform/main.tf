955dedaf5ac08582176b6597d3bf9e56af70880f
yanibnk <135028223+yanibnk@users.noreply.github.com>
2025-05-07 10:31:23 +0000

    try5

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,17 +5,34 @@ provider "aws" {
   skip_requesting_account_id  = true
   skip_metadata_api_check     = true
   skip_credentials_validation = true
+
   endpoints {
-    ec2 = "http://ip10-0-3-4-d0dgq9805akh4glkf8og-4566.direct.lab-boris.fr"
+    ec2 = "http://ip10-0-3-4-d0dgq9805akh4glkf8og-4566.direct.lab-boris.fr/"
   }
 }
 
-variable "image_id" {
-  description = "AMI ID"
-  type        = string
+Génère un ID aléatoire à chaque apply (force nouvelle ressource)
+resource "random_id" "commit_simulation" {
+  keepers = {
+    always_change = timestamp() # nouvelle valeur à chaque apply
+  }
+
+  byte_length = 4
 }
 
 resource "aws_instance" "demo" {
-  ami           = var.image_id
+  ami           = "ami-${random_id.commit_simulation.hex}" # fictif mais uniquejbjjkbbijbjk
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "Instance-${random_id.commit_simulation.hex}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+output "created_instance_id" {
+  value = aws_instance.demo.id
 }
