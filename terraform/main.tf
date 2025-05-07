a6acbed1bd55ad75c732d72efc1d82b29578c4cd
yanibnk <135028223+yanibnk@users.noreply.github.com>
2025-05-07 09:40:50 +0000

    try

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  skip_credentials_validation = true
+  endpoints {
+    ec2 = "http://ip10-0-3-4-d0dgq9805akh4glkf8og-4566.direct.lab-boris.fr"
+  }
+}
+
+resource "aws_instance" "demo" {
+  ami           = "ami-12345678"
+  instance_type = "t2.micro"
+}
